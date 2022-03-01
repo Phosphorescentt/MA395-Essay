f09ff92021a4f588f6f70f53c1a6a330d083bb18
Joshua Mankelow <joshua@mankelow.org>
2022-03-01 12:16:19 +0000

    Wrote some more stuff for section 2

--- code/section1examples/simple_example.py
+++ code/section1examples/simple_example.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 G = nx.Graph()
 
 G.add_node(1, pos=(0, 0))
-G.add_node(2, pos=(50, 50))
-G.add_node(3, pos=(50, -50))
-G.add_node(4, pos=(100, 0))
-G.add_node(5, pos=(150, 0))
+G.add_node(2, pos=(1, 1))
+G.add_node(3, pos=(1, -1))
+G.add_node(4, pos=(2, 0))
+G.add_node(5, pos=(3, 0))
 
 G.add_edge(1, 2)
 G.add_edge(1, 3)
@@ -25,7 +25,8 @@ G.add_edge(4, 5)
 
 pos = nx.get_node_attributes(G, "pos")
 
-nx.draw(G, pos, node_color="#ffffff", node_size=500, with_labels=True)
+nx.draw(G, pos, node_color="#ffffff", node_size=2500, with_labels=True, font_size=32)
+# nx.draw_networkx_labels(G, pos, font_size=200)
 
 ax = plt.gca()
 ax.collections[0].set_edgecolor("#000000")
